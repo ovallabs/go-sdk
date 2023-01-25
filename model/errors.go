c4a2ab1c8f1c84b8d069922c1eb62913c30f9396
Olawale Oladapo S <walex337@gmail.com>
2023-01-25 15:00:46 +0100

    update error response

--- model/errors.go
+++ model/errors.go
@@ -7,5 +7,5 @@ import (
 var (
 	// ErrNetworkError when something goes wrong with the API call
 	ErrNetworkError        = errors.New("network error")
-	ErrUnsupportedCurrency = errors.New("currency is not a supported currency")
+	ErrServiceNotAvailable = errors.New("this service is currently unavailable. Please try again later")
 )
