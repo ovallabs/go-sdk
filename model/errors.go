fef74a67a6b4f65a678f2838bbcbb181615a983a
Olawale Oladapo S <walex337@gmail.com>
2023-01-25 14:48:45 +0100

    feat: update error response on getExchangeRate details

--- model/errors.go
+++ model/errors.go
@@ -1,8 +1,11 @@
 package model
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	// ErrNetworkError when something goes wrong with the API call
-	ErrNetworkError = errors.New("network error")
+	ErrNetworkError        = errors.New("network error")
+	ErrUnsupportedCurrency = errors.New("currency is not a supported currency")
 )
