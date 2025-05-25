e30e279e6b6433d30068f28c145227ad492e1547
Afunnilo <afunnilo@gmail.com>
2025-05-25 11:52:06 +0100

    Expose endpoint to submit documents on go sdk

--- api/kyc.go
+++ api/kyc.go
@@ -3,12 +3,10 @@ package api
 import (
 	"context"
 	"fmt"
-	"net/http"
-
 	"github.com/ovalfi/go-sdk/model"
+	"net/http"
+	"os"
 )
-
-const kycAPIVersion = "v1/kycs"
 
 // GetKYCByCustomerID makes request to get KYC for a customer
 func (c *Call) GetKYCByCustomerID(ctx context.Context, customerID string) (model.KYCResponse, error) {
@@ -19,6 +17,42 @@ func (c *Call) GetKYCByCustomerID(ctx context.Context, customerID string) (model
 	)
 
 	err = c.makeRequest(ctx, path, http.MethodGet, nil, nil, nil, nil, &response)
+
+	return response, err
+}
+
+func (c *Call) SubmitKYCDocuments(ctx context.Context, customerID string) (model.KYCResponse, error) {
+	var (
+		err      error
+		response model.KYCResponse
+		path     = fmt.Sprintf("%s/%s", kycAPIVersion, customerID)
+	)
+
+	err = c.makeRequest(ctx, path, http.MethodPost, nil, nil, nil, nil, &response)
+
+	return response, err
+}
+
+func (c *Call) SubmitCustomerKYCDocument(
+	ctx context.Context,
+	customerID string,
+	document *os.File,
+	documentType string,
+	country string,
+) (model.KYCResponse, error) {
+	var (
+		err      error
+		response model.KYCResponse
+		formData = make(map[string]interface{})
+		path     = fmt.Sprintf("%s/%s/document-verification", kycAPIVersion, customerID)
+	)
+
+	// Prepare form data
+	formData["document"] = document
+	formData["documentType"] = documentType
+	formData["country"] = country
+
+	err = c.makeRequest(ctx, path, http.MethodPost, nil, nil, formData, nil, &response)
 
 	return response, err
 }
