df78b700b36cb7824ad8bf9d662279a8e75681f9
Raji Victor <vheektoure@gmail.com>
2025-11-10 10:16:16 +0100

    Add get wallet address on go sdk

---
+++ api/crypto.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/ovalfi/go-sdk/model"
+)
+
+const cryptoAPIVersion = "v1/crypto"
+
+// GetCustomerWallet makes request to Torus to get customer wallet if available or  create customer wallet if not
+func (c *Call) GetCustomerWallet(ctx context.Context, request model.CustomerWalletRequest) (model.CustomerWallet, error) {
+	var (
+		err      error
+		response model.CustomerWallet
+		params   = map[string]interface{}{"customer_id": request.CustomerID, "network": request.Network, "asset": request.Asset}
+		path     = fmt.Sprintf("%s/wallet", cryptoAPIVersion)
+	)
+
+	err = c.makeRequest(ctx, path, http.MethodGet, nil, params, nil, nil, &response)
+
+	return response, err
+}
